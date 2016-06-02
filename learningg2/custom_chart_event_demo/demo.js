cb0f827678a126a26a38704a8b1e499116ecbabd
DoubleDeckers <seer_buaasoft2015@163.com>
2016-06-03 01:48:30 +0800

    增加一个新的Demo

---
+++ learningg2/custom_chart_event_demo/demo.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Obscurity on 2016/5/28.
+ */
+
+(function () {
+    $(document).ready(function () {
+        var html_dom = $("<div></div>");
+        $("body").prepend(html_dom);
+        var data = [
+            {action: '访问', visitor: 500},
+            {action: '浏览', visitor: 400},
+            {action: '交互', visitor: 300},
+            {action: '下单', visitor: 200},
+            {action: '付款', visitor: 100}
+        ];
+        var chart = new G2.Chart({
+            container: html_dom.get(0),
+            width : 800,
+            height : 400,
+            animate: true
+        });
+        chart.source(data);
+        chart.interval().position('action*visitor').color('action'); // create the detail chart
+        chart.coord('rect');
+        chart.tooltip(true, {
+            map: {
+                title: 'visitor',
+                name: '访问量',
+                value: 'visitor'
+            }
+        });
+        chart.render();
+
+        chart.on('plotenter', function (event) {
+            $("body").css("cursor", "pointer");
+        });
+
+        chart.on('plotleave', function (event) {
+            $("body").css("cursor", "auto");
+        });
+
+    })
+})();
