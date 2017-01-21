d44cb2e21833cfa404c37fe3d2fbf80941fb8b00
HeavenDuke <trashlhc@163.com>
2017-01-21 18:42:34 +0800

    增加了一个demo

--- hshare/test.js
+++ hshare/test.js
@@ -15,3 +15,31 @@ $("#container").hshare({
         text: "豆瓣"
     }]
 });
+
+$("#container2").hshare({
+    size: "medium",
+    hasText: true,
+    shares: [{
+        platform: "qzone",
+        icon: "https://qzonestyle.gtimg.cn/ac/qzone_v5/app/app_share/qz_logo.png",
+        text: "分享到QQ空间"
+    }, {
+        platform: "douban",
+        icon: "https://img3.doubanio.com/pics/fw2douban_s.png",
+        text: "豆瓣"
+    }]
+});
+
+$("#container3").hshare({
+    size: "small",
+    hasText: true,
+    shares: [{
+        platform: "qzone",
+        icon: "https://qzonestyle.gtimg.cn/ac/qzone_v5/app/app_share/qz_logo.png",
+        text: "分享到QQ空间"
+    }, {
+        platform: "douban",
+        icon: "https://img3.doubanio.com/pics/fw2douban_s.png",
+        text: "豆瓣"
+    }]
+});
