1266a165089719e44cbeca7db94a93dd6d8585ac
HeavenDuke <trashlhc@163.com>
2017-01-21 18:32:05 +0800

    增加了一个demo

--- hshare/test.js
+++ hshare/test.js
@@ -10,6 +10,8 @@ $("#container").hshare({
         icon: "https://qzonestyle.gtimg.cn/ac/qzone_v5/app/app_share/qz_logo.png",
         text: "分享到QQ空间"
     }, {
-        platform: "douban"
+        platform: "douban",
+        icon: "https://img3.doubanio.com/pics/fw2douban_s.png",
+        text: "豆瓣"
     }]
 });
