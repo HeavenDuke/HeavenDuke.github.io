663cca1f208d301c176c31e9393b1b90c6dd80bd
HeavenDuke <trashlhc@163.com>
2017-01-21 18:20:38 +0800

    Signed-off-by: HeavenDuke <trashlhc@163.com>

---
+++ hshare/test.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Obscurity on 2017/1/21.
+ */
+
+$("#container").hshare({
+    size: "large",
+    hasText: true,
+    shares: [{
+        platform: "qzone",
+        icon: "https://qzonestyle.gtimg.cn/ac/qzone_v5/app/app_share/qz_logo.png",
+        text: "分享到QQ空间"
+    }]
+});
