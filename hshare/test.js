7d7ac86cdc51c906fd895d17f97ca0bdbff3ea80
HeavenDuke <trashlhc@163.com>
2017-01-21 23:22:44 +0800

    增加了一个demo

--- hshare/test.js
+++ hshare/test.js
@@ -4,28 +4,28 @@
 
 $("#container").hshare({
     size: "large",
-    shares: [{
-        platform: "qzone",
+    platforms: [{
+        name: "qzone",
         icon: "https://qzonestyle.gtimg.cn/ac/qzone_v5/app/app_share/qz_logo.png",
         text: "分享到QQ空间"
     }, {
-        platform: "qq",
+        name: "qq",
         icon: "https://open.weixin.qq.com/zh_CN/htmledition/res/assets/res-design-download/icon64_appwx_logo.png",
         text: "QQ好友"
     }, {
-        platform: "douban",
+        name: "douban",
         icon: "https://img3.doubanio.com/pics/fw2douban_s.png",
         text: "豆瓣"
     }, {
-        platform: "renren",
+        name: "renren",
         icon: "http://dev.renren.com/views/website/inc/images/share08.png",
         text: "人人网"
     }, {
-        platform: "sinaweibo",
+        name: "sinaweibo",
         icon: "https://img.t.sinajs.cn/t4/appstyle/widget/images/shareButton/share_icon_mini.png?id=1375775002754",
         text: "新浪微博"
     }, {
-        platform: "wechat",
+        name: "wechat",
         icon: "https://open.weixin.qq.com/zh_CN/htmledition/res/assets/res-design-download/icon64_appwx_logo.png",
         text: "微信"
     }]
@@ -33,28 +33,27 @@ $("#container").hshare({
 
 $("#container2").hshare({
     size: "medium",
-    renderText: true,
-    shares: [{
-        platform: "qzone",
+    platforms: [{
+        name: "qzone",
         text: "分享到QQ空间"
     }, {
-        platform: "qq",
+        name: "qq",
         icon: "https://open.weixin.qq.com/zh_CN/htmledition/res/assets/res-design-download/icon64_appwx_logo.png",
         text: "QQ好友"
     }, {
-        platform: "douban",
+        name: "douban",
         icon: "https://img3.doubanio.com/pics/fw2douban_s.png",
         text: "豆瓣"
     }, {
-        platform: "renren",
+        name: "renren",
         icon: "http://dev.renren.com/views/website/inc/images/share09.png",
         text: "人人网"
     }, {
-        platform: "sinaweibo",
+        name: "sinaweibo",
         icon: "https://img.t.sinajs.cn/t4/appstyle/widget/images/shareButton/share_icon_mini.png?id=1375775002754",
         text: "新浪微博"
     }, {
-        platform: "wechat",
+        name: "wechat",
         icon: "https://open.weixin.qq.com/zh_CN/htmledition/res/assets/res-design-download/icon64_appwx_logo.png",
         text: "微信"
     }]
@@ -62,29 +61,28 @@ $("#container2").hshare({
 
 $("#container3").hshare({
     size: "small",
-    renderText: true,
-    shares: [{
-        platform: "qzone",
+    platforms: [{
+        name: "qzone",
         icon: "https://qzonestyle.gtimg.cn/ac/qzone_v5/app/app_share/qz_logo.png",
         text: "分享到QQ空间"
     }, {
-        platform: "qq",
+        name: "qq",
         icon: "https://open.weixin.qq.com/zh_CN/htmledition/res/assets/res-design-download/icon64_appwx_logo.png",
         text: "QQ好友"
     }, {
-        platform: "douban",
+        name: "douban",
         icon: "https://img3.doubanio.com/pics/fw2douban_s.png",
         text: "豆瓣"
     }, {
-        platform: "renren",
+        name: "renren",
         icon: "http://dev.renren.com/views/website/inc/images/share10.png",
         text: "人人网"
     }, {
-        platform: "sinaweibo",
+        name: "sinaweibo",
         icon: "https://img.t.sinajs.cn/t4/appstyle/widget/images/shareButton/share_icon_mini.png?id=1375775002754",
         text: "新浪微博"
     }, {
-        platform: "wechat",
+        name: "wechat",
         icon: "https://open.weixin.qq.com/zh_CN/htmledition/res/assets/res-design-download/icon64_appwx_logo.png",
         text: "微信"
     }]
